8eb11a6e7cf79eacc91eba4cab42c7d9962cc891
pao-pipat <nichakorn.pipat@gmail.com>
2024-10-17 15:23:39 +0100

    more test on minimum function

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,4 +16,4 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
-    assert ci_course.minimum("Hello", 3) is None
+    assert ci_course.minimum("Hello", "Hi") is None
